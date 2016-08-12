66f6c35431d3a62e6887e98ad770de65203a95a8
Bas <bastiheerwald@live.de>
2016-08-12 21:27:56 +0200

    Fixed script to flash firmware.

--- flash_firmware.sh
+++ flash_firmware.sh
@@ -1,1 +1,2 @@
-sudo python esptool.py -p $1 write_flash 0x00 esp8266-firmware/build.nodemcu/esp8266-firmware.bin
+cd esptool
+sudo python esptool.py -p $1 write_flash 0x00 ../esp8266-firmware/build.nodemcu/esp8266-firmware.bin
