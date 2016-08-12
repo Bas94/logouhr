c4af53f9fdcd8b787e9eda99710298bcee4e0411
Bas <bastiheerwald@live.de>
2016-08-12 21:20:30 +0200

    Added versio of install script wit correct tags of dependencies.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-#git clone --recursive https://github.com/thunderace/Esp8266-Arduino-Makefile.git dependencies
-#cd dependencies
-#./instal-x86_64-pc-linux-gnul.sh
-#cd ..
+git clone --recursive https://github.com/thunderace/Esp8266-Arduino-Makefile.git dependencies
+cd dependencies
+git checkout 88493e7ba55b9ea4178b86b9b6e6b9f1f7034eed
+./install-x86_64-pc-linux-gnu.sh
+cd libraries
+git clone https://github.com/adafruit/Adafruit_NeoPixel.git
+cd Adafruit_NeoPixel
+git checkout v1.0.6
+cd ../../..
 git clone https://github.com/themadinventor/esptool.git
